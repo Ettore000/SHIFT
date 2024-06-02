553e76d5c5279e69d2031bfcb482648ac067af23
Ettore000 <ettore.panasia@gmail.com>
2024-06-02 19:54:23 +0200

    Update PaymentSendDB.js

--- PaymentSendDB.js
+++ PaymentSendDB.js
@@ -1,5 +1,7 @@
 db.posts.insertOne({
-    date: Date(),
-    id_customer: "[put the user id]",
-    id_bicycles: "[put the bike id]"
+    res_day: 10,
+    res_month: 5,
+    res_year: 2024,
+    id_customer: "1",
+    id_bicycles: "5"
 })
